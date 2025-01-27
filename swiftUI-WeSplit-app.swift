5add5f2c6610e84e28058acadc43b2654f811609
Noor Mahammad Chervu <noormahammad@users.noreply.github.com>
2025-01-26 21:21:29 -0500

    Create swiftUI-WeSplit-app.swift
    A simple check split app showcasing different types of picker controls

---
+++ swiftUI-WeSplit-app.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by noor on 1/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var isAmountFocused: Bool
+    var tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double {
+        var peopleCount = Double(numberOfPeople + 2)
+        var tipSelection = Double(tipPercentage)
+        
+        var tipValue = checkAmount/100 * tipSelection
+        var grandTotal = checkAmount + tipValue
+        var amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format:
+                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($isAmountFocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    //.pickerStyle(.navigationLink)
+                }
+                
+                Section("How much do you like to tip?") {
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format:.percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if(isAmountFocused) {
+                    Button("Done") {
+                        isAmountFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
