1f93f19c24802a7a856c413b18fdffc4e9235c70
Noor Mahammad Chervu <noormahammad@users.noreply.github.com>
2025-02-05 08:06:43 -0500

    Create swiftUI-LengthConvert-app.swift
    simple length conversion between Meters, Kilometers, Foot and Yard using computed properties, state property wrappers, Picket, TextField controls

---
+++ swiftUI-LengthConvert-app.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  Simple Bank Account app using Swift UI
+//
+//  Created by Noor on 2/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var inputUnit = 1.0
+    @State var inputUnitType = "Meter"
+    @State var outputUnitType = "Foot"
+    
+    //first convert input into lowest unit type
+    var inputUnitInFeet : Double  {
+        var feet: Double;
+        switch inputUnitType {
+        case "Meter":
+            feet = inputUnit * 3.28084
+        case "Kilometer":
+            feet = inputUnit * 3280.84
+        case "Yard":
+            feet = inputUnit * 3
+        default:
+            feet = inputUnit
+        }
+        return feet
+    }
+    
+    var outputUnit: Double {
+        var output = 1.0
+        switch outputUnitType {
+        case "Meter":
+            output = inputUnitInFeet / 3.28084
+        case "Kilometer":
+            output = inputUnitInFeet / 3280.84
+        case "Yard":
+            output = inputUnitInFeet / 3
+        default:
+            output = inputUnitInFeet
+        }
+        return output
+    }
+    
+    let unitTypes = ["Meter", "Kilometer", "Foot", "Yard"]
+  
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section() {
+                        TextField("Enter a number", value:$inputUnit, format:.number)
+                        Picker("", selection: $inputUnitType){
+                            ForEach(unitTypes, id:\.self) {
+                                Text("\($0)")
+                            }
+                        }.pickerStyle(.segmented)
+                }
+                Section("") {
+              
+                        Text("\(outputUnit)")
+                        Picker("", selection: $outputUnitType){
+                            ForEach(unitTypes, id:\.self) {
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    
+                }
+                Section() {
+                    
+                }
+                
+            }
+            .navigationTitle("Length Conversion")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
